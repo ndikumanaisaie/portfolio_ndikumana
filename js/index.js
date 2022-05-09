12e6d463a29d7c4744662c52c4dd59000f787047
ndikumanaisaie <54780027+ndikumanaisaie@users.noreply.github.com>
2022-05-09 23:50:58 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 const humberger = document.getElementById('hamburger-icon');
-humberger.addEventListener ('click', () => {
-  humberger.classList.toggle ('open');
+  humberger.addEventListener('click', () => {
+  humberger.classList.toggle('open');
 });
